b1b9a21c3cbf6ee476d49da36c070285e1f64538
Travis <travisok2003@gmail.com>
2026-01-27 15:20:37 +0100

    Home Page UI WIP

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,40 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatelessWidget {
 
   const Home ({super.key});
 
   @override Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         body: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.all(12.0),
+              padding: const EdgeInsets.fromLTRB(25, 65, 25, 0),
               child: Column(
                 children: [
                   Container(
-                    height: 120,
+                    width: double.infinity,
+                    // height: 160,
                     decoration: BoxDecoration(
                       color: Color(0xFF008080),
                       borderRadius: BorderRadius.circular(12)
                     ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(17.5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Monthly Summary',
+                            style: GoogleFonts.inter(
+                              fontSize: 11.5,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400
+                            ),
+                          ),
+                          SizedBox(height: 23,),
+                          Text(
+                            'Total:',
+                            style: GoogleFonts.inter(
+                              fontSize: 17.5,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400
+                            ),
+                          ),
+                          SizedBox(height: 29,),
+                          Container(
+                            height: 33.5,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Color(0xFFD1D1D1),
+                              borderRadius: BorderRadius.circular(12)
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Pay now',
+                                style: GoogleFonts.inter(
+                                  fontSize: 14.5,
+                                  color: Color(0xFF008080),
+                                  fontWeight: FontWeight.w700
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
                   ),
                   SizedBox(height: 12),
                   Row(
                     children: [
-                      Container(
-                        height: 90,
-                        decoration: BoxDecoration(
-                          color: Color(0xFFA0FFFF),
-                          borderRadius: BorderRadius.circular(12)
+                      Expanded(
+                        child: Container(
+                          height: 90,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFA0FFFF),
+                            borderRadius: BorderRadius.circular(12)
+                          ),
                         ),
                       ),
-                      Container(
-                        height: 90,
-                        decoration: BoxDecoration(
-                          color: Color(0xFFA0FFFF),
-                          borderRadius: BorderRadius.circular(12)
+                      Expanded(
+                        child: Container(
+                          height: 90,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFA0FFFF),
+                            borderRadius: BorderRadius.circular(12)
+                          ),
                         ),
                       ),
                     ],
@@ -44,7 +93,6 @@ class Home extends StatelessWidget {
             )
           ],
         )
-      )
     );
   }
 }
